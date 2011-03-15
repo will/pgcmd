8e5cfe46b64879a2d3357799fcffe4d05dfd8fd8
Will Leinweber <will@bitfission.com>
2011-03-14 18:44:52 -0700

    some first tries

--- init.rb
+++ init.rb
@@ -2,6 +2,31 @@ module Heroku
   module Command
     class Pg
 
+      def config_vars
+        raise 'not implemented'
+      end
+
+      def resolve(db_id)
+        db_id = url_deprecation_check(db_id)
+        if db_id == 'DATABASE'
+          db_id = 'SHARED_DATABASE'
+          heroku.display('using SHARED_DATABASE_URL')
+        end
+        db_var = "#{db_id}_URL"
+        config_vars[db_var]
+      end
+
+      private
+
+      def url_deprecation_check(db_id)
+        pattern = /_URL$/
+        if db_id =~ pattern
+          old_id = db_id.dup
+          db_id.gsub!(pattern,'')
+          heroku.display("#{old_id} is deprecated, please use #{db_id}")
+        end
+        db_id
+      end
     end
   end
 end
--- pg_spec.rb
+++ pg_spec.rb
@@ -5,5 +5,47 @@ require 'init'
 include Heroku::Command
 
 describe Pg, '#resolve' do
-  it 'should be awesome'
+  let(:pg) do
+    pg = Pg.new
+    pg.stub!(:heroku).and_return(mock(:heroku))
+    pg
+  end
+
+  let(:heroku) { pg.heroku }
+
+  def should_display(msg)
+    heroku.should_receive(:display) { |arg| arg.should =~ /#{msg}/ }
+  end
+
+# RED
+# HEROKU_POSTGRESQL_RED => SWITCH TO RED
+# HEROKU_POSTGRESQL_RED_URL => warn, switch to RED
+# DATABSE_URL => warn, switch to database
+#
+  context "pass in *_URL" do
+    it 'should warn to not add in _URL, and proceed without it' do
+      pg.stub!(:config_vars).and_return({"SOME_URL" => 'something'})
+      should_display("SOME_URL is deprecated, please use SOME")
+      pg.resolve("SOME_URL").should == 'something'
+    end
+  end
+
+  context "only shared database" do
+    before(:each) do
+      pg.stub!(:config_vars).and_return({
+        'DATABASE_URL' => 'postgres://shared',
+        'SHARED_DATABASE_URL' => 'postgres://shared',
+      })
+    end
+
+    it 'returns the shared url when asked for DATABASE' do
+      should_display "SHARED_DATABASE_URL"
+      pg.resolve("DATABASE").should == 'postgres://shared'
+    end
+
+    it 'reutrns the shared url when asked for SHARED_DATABASE' do
+      heroku.should_not_receive(:display)
+      pg.resolve("SHARED_DATABASE").should == 'postgres://shared'
+    end
+  end
 end
