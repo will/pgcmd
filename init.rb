c4656ca7c2e17a0dfc969539b7517c05a57a3d69
Will Leinweber <will@bitfission.com>
2011-03-18 18:07:18 -0700

    how about actually resolving databases?

--- init.rb
+++ init.rb
@@ -4,8 +4,12 @@ module Heroku
       alias configure_addon_without_pg configure_addon
       def configure_addon(label, &install_or_upgrade)
         %w[fork track].each do |opt|
-          val = extract_option("--#{opt}")
-          args << "#{opt}=#{val}" if val
+          if val = extract_option("--#{opt}")
+            db = Pg::Resolver.new(val, heroku.config_vars(app))
+            display db.message if db.message
+            abort("Could not resolve database #{db[:name]}") unless db[:url]
+            args << "#{opt}=#{db[:url]}"
+          end
         end
         configure_addon_without_pg(label, &install_or_upgrade)
       end
